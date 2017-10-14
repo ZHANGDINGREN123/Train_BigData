b78eaa2d139a08589fccdb5dc7a7617c16707173
zhangdingding <zhangdingding@master.com>
2017-10-14 21:13:38 +0800

    初始化

---
+++ BigData.py
@@ -0,0 +1,25 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+@app.route('/123')
+def hello1():
+    return render_template("calendar.html")
+
+@app.route('/1234')
+def hello2():
+    return render_template("chartjs.html")
+
+@app.route("/bigData")
+def big():
+    return "bigData"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=8888)
+
